3b5b83fd4b70089238d819bc3f59b43f973a6dcb
ChaseKnowlden <knowlden887@gmail.com>
2021-03-12 19:17:28 -0500

    Add Tutorial 3
    Data Types and Variables

---
+++ tutorial3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    x = 7;
+    x = 6;
+    x = 8;
+    y = 4;
+    x = y;
+    cout << x;
+    string name = "eric";
+    cout << name;
+}
+
+/*
+int 7, 98, -9, -9765, 0, 10
+float_denorm_style 7.0, -9.8, 0.2, 2.1
+bool true, false
+string ""
+char 'x' '0' 's'
+*/
